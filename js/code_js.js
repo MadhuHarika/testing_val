f49d73307c9bd8bb04cb6fce22381d6e2038bbc8
MadhuNandipati <nandipati.madhuharika34@gmail.com>
2020-09-29 17:25:43 +0530

    fourth commit

--- js/code_js.js
+++ js/code_js.js
@@ -1,33 +1,29 @@
 function valfname()
 {
 
-   var regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+   var regName = /^[A-Za-z]+$/;
     var name = document.getElementsByName('FirstName').value;
+    console.log(name);
     if(!regName.test(name)){
         alert('Enter alphabets only!!!');
-    }else{
-        alert('Valid name given.');
     }
-
-}
+    }
 
 function vallname()
 {
-    var regName=/^[a-zA-Z]+ [a-zA-Z]+$/;
+    var regName=/^[A-Za-z]+$/;
     var name = document.getElementsByName('LastName').value;
     if(!regName.test(name)){
         alert('Enter alphabets only!!!');
-    }else{
-        alert('Valid name given.');
     }
 
 }
 function valUname()
 {
-    var regName=/^[A-Za-z0-9_@\.]|@{2,}|\.{5,};
+    var regName=/^[A-Za-z0-9_@\.]|@{2,}|\.{5,}/;
     var name=document.getElementsByName('UserName').value;
     var len=name.length;
-    let at = document.getElementsByName('UserName').value.indexOf("[0-9]")
+    let at = document.getElementsByName('UserName').value.indexOf("[0-9]");
     if(len(name)<8 || len(name)>12)
     {
         alert('Length of username should be between 8 & 12 ');
